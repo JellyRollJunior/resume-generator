b998be8117d0b20b5a21ca80387843e61fcfabd9
Brandon Lin <brandonlin9716@gmail.com>
2024-12-19 19:31:06 -0800

    Move form and output into ResumeGenerator component

--- src/components/ResumeGenerator.jsx
+++ src/components/ResumeGenerator.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import { ResumeForm } from "./ResumeForm";
-import { ResumeOutput } from "./ResumeOutput";
-export { ResumeGenerator }
+export { ResumeGenerator };
 
 const ResumeGenerator = () => {
   const [generalInfo, setGeneralInfo] = useState({
@@ -11,10 +9,101 @@ const ResumeGenerator = () => {
     email: 'supa_arubaito_shisa@chiikawa.co',
   });
 
+  const handleFirstname = (event) => [
+    setGeneralInfo({
+      ...generalInfo,
+      firstname: event.target.value,
+    }),
+  ];
+
+  const handleLastname = (event) => [
+    setGeneralInfo({
+      ...generalInfo,
+      lastname: event.target.value,
+    }),
+  ];
+
+  const handlePhone = (event) => [
+    setGeneralInfo({
+      ...generalInfo,
+      phone: event.target.value,
+    }),
+  ];
+
+  const handleEmail = (event) => [
+    setGeneralInfo({
+      ...generalInfo,
+      email: event.target.value,
+    }),
+  ];
+
   return (
     <div>
-      <ResumeForm generalInfo={generalInfo} setGeneralInfo={setGeneralInfo} />
-      <ResumeOutput generalInfo={generalInfo} />
+      <section>
+        <form action="">
+          <h1>Resume Generator</h1>
+          <fieldset>
+            <legend>General Information</legend>
+            <div>
+              <label htmlFor="firstname">Firstname: </label>
+              <input
+                type="text"
+                name="firstname"
+                id="firstname"
+                value={generalInfo.firstname}
+                onChange={handleFirstname}
+              />
+            </div>
+            <div>
+              <label htmlFor="lastname">Lastname: </label>
+              <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                value={generalInfo.lastname}
+                onChange={handleLastname}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone">Phone Number: </label>
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                value={generalInfo.phone}
+                onChange={handlePhone}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email: </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={generalInfo.email}
+                onChange={handleEmail}
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Education</legend>
+          </fieldset>
+          <fieldset>
+            <legend>Work Experience</legend>
+          </fieldset>
+        </form>
+      </section>
+      <section>
+        <h2>Generated Resume</h2>
+        <div>
+          <h3>
+            {generalInfo.firstname} {generalInfo.lastname}
+          </h3>
+          <h4>
+            {generalInfo.phone} | {generalInfo.email}
+          </h4>
+        </div>
+      </section>
     </div>
   );
-}
+};
