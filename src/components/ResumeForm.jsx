35ba19e21379ef56fe5523fd699209884f68252a
Brandon Lin <brandonlin9716@gmail.com>
2024-12-19 16:10:03 -0800

    Create inputs for general information

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -3,8 +3,28 @@ export { ResumeForm };
 const ResumeForm = () => {
   return (
     <section>
-      <h1>Resume Generator</h1>
-      <form action=""></form>
+      <form action="">
+        <h1>Resume Generator</h1>
+        <fieldset>
+          <legend>General Information</legend>
+          <div>
+            <label htmlFor="firstName">Firstname: </label>
+            <input type="text" name="firstName" id="firstName" />
+          </div>
+          <div>
+            <label htmlFor="lastName">Lastname: </label>
+            <input type="text" name="lastName" id="lastName" />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone Number: </label>
+            <input type="tel" name="phone" id="phone" />
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" id="email" />
+          </div>
+        </fieldset>
+      </form>
     </section>
   );
 };
