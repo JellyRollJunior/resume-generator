69282a9fab966f62978d301ba49216645e5101ed
Brandon Lin <brandonlin9716@gmail.com>
2024-12-19 16:51:51 -0800

    Add initial values to general info inputs

--- src/components/ResumeForm.jsx
+++ src/components/ResumeForm.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
 export { ResumeForm };
 
 const ResumeForm = () => {
+  const [generalInfo, setGeneralInfo] = useState({
+    firstName: 'Shisa',
+    lastName: 'Chiikawa',
+    phone: '888-888-8888',
+    email: 'supa_arubaito_shisa@chiikawa.co',
+  });
   return (
     <section>
       <form action="">
@@ -9,26 +17,46 @@ const ResumeForm = () => {
           <legend>General Information</legend>
           <div>
             <label htmlFor="firstName">Firstname: </label>
-            <input type="text" name="firstName" id="firstName" />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={generalInfo.firstName}
+            />
           </div>
           <div>
             <label htmlFor="lastName">Lastname: </label>
-            <input type="text" name="lastName" id="lastName" />
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              value={generalInfo.lastName}
+            />
           </div>
           <div>
             <label htmlFor="phone">Phone Number: </label>
-            <input type="tel" name="phone" id="phone" />
+            <input
+              type="tel"
+              name="phone"
+              id="phone"
+              value={generalInfo.phone}
+            />
           </div>
           <div>
             <label htmlFor="email">Email: </label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={generalInfo.email}
+            />
           </div>
         </fieldset>
         <fieldset>
-            <legend>Education</legend>
+          <legend>Education</legend>
         </fieldset>
         <fieldset>
-            <legend>Work Experience</legend>
+          <legend>Work Experience</legend>
         </fieldset>
       </form>
     </section>
